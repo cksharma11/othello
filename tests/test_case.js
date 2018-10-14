8ec1aab0e1785d883bc7bda3e99bd4a3aec93d86
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-10-14 19:25:08 +0530

    Modified test cases for printBoard()

--- tests/test_case.js
+++ tests/test_case.js
@@ -25,16 +25,16 @@ assert.deepEqual(engineLib.printBoard(board), output);
 
 //Test for 1x1 board
 board = [["-"]];
-output = "-";
+output = "- ";
 assert.deepEqual(engineLib.printBoard(board), output);
 
 //Test for 2x2 board
 board = [["-","-"], ["-", "-"]];
-output = "--\n--";
+output = "- - \n- - ";
 assert.deepEqual(engineLib.printBoard(board), output);
 
 //Test for 4x4 board
 board = [["-","-","-","-"], ["-", "-", "-", "-"]];
-output = "----\n----\n----\n----";
+output = "- - - - \n- - - - \n- - - - \n- - - - ";
 assert.deepEqual(engineLib.printBoard(board), output);
 

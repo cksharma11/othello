dad6996800f02f00c97c017a4a3073361ff1a82a
naman Sharma <44020825+namankrs@users.noreply.github.com>
2018-10-14 18:46:48 +0530

    added a function 'generateBoard'
    it generates a square board taking side as a input.

--- libs/core.js
+++ libs/core.js
@@ -1,1 +1,13 @@
-//Generic functions
+//this function generates a block of given side dimension
+const generateBoard = function(side){
+  let board = [];
+  for(let row=0; row<side; row++){
+    board[row] = [];
+    for(let column=0; column<side; column++){
+      board[row][column] = "-"
+    }
+  }
+return board;
+}
+exports.generateBoard = generateBoard;
+

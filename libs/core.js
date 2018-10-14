5e4452e5ee95b7d3084fbfcc0ac23dd82312741f
naman Sharma <44020825+namankrs@users.noreply.github.com>
2018-10-14 19:08:15 +0530

    code refactor

--- libs/core.js
+++ libs/core.js
@@ -1,4 +1,4 @@
-//this function generates a block of given side dimension
+//this function generates a board of given side dimension
 const generateBoard = function(side){
   let board = [];
   for(let row=0; row<side; row++){

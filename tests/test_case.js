c69b5c994ff6fe4d3874edc3b2d29069da88c56b
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-10-14 18:40:04 +0530

    Added test case for generateBoard()

--- tests/test_case.js
+++ tests/test_case.js
@@ -1,1 +1,16 @@
-//Empty test case
+const assert = require("assert");
+const engineLib = require("../libs/engine.js");
+const coreLib = require("../libs/core.js");
+
+//---------------------------Test for generateBoard() function
+//Test for input 0
+assert.deepEqual(coreLib.generateBoard(0), []);
+
+//Test for input 1
+assert.deepEqual(coreLib.generateBoard(1), [["-"]]);
+
+//Test for input 2
+assert.deepEqual(coreLib.generateBoard(2), [["-", "-"], ["-", "-"]]);
+
+//Test for input 4
+assert.deepEqual(coreLib.generateBoard(4), [["-", "-", "-", "-"], ["-", "-", "-", "-"],["-", "-", "-", "-"],["-", "-", "-", "-"]]);
